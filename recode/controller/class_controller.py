1628a556705536a2756dec563138d5e34c70525c
axiqia <axiqia@qq.com>
2020-08-06 15:33:32 +0800

    other

--- recode/controller/class_controller.py
+++ recode/controller/class_controller.py
@@ -16,7 +16,7 @@ class node_controller():
     def __init__(self,node_id):
         self.node_id = node_id
         self.repack_controller = repack_controller()
-        self.action_list = {} #{"renter A":A_obj}
+        self.action_list = {} #{"renter A":xxx}
         self.renter_lender_info = {} #{"renter A": {lender B: cos, lender C:cos}}
     
     def action_info_update(self,action_obj=None):
@@ -40,21 +40,25 @@ class node_controller():
         self.repack_controller.packages_reload()
         lender,renter = self.repack_controller.action_repack(action_name,docker_file_json,packages_json,share_action_number)
         self.repack_controller.image_save()
-        #inform intra to generate lender container
-        self.renter_lender_info_add(lender,renter)
+        #inform intra 
         return lender, renter
 
     def action_scheduler(self,action_obj):
         if action_obj.action_name in self.renter_lender_info.keys():
             lender_obj = max(self.renter_lender_info[action_obj.action_name], key=self.renter_lender_info[action_obj.action_name].get) 
             if (self.action_list[lender_obj].share_count > 0) and (action_obj.current_containers < action_obj.max_containers):
-                action_obj.renter_instance_info = self.action_list[lender_obj].lender_instance_info[self.action_list[lender_obj].share_count-1]
-                #加容器状态检查
-                self.action_list[lender_obj].lender_instance_info[self.action_list[lender_obj].share_count-1] = None
-                self.action_list[lender_obj].share_count -= 1
-                self.action_list[lender_obj].current_containers -= 1
-                action_obj.current_containers += 1
-        else:pass
+                #inform lender delete container
+                #inform renter add container
+                
+                # action_obj.renter_instance_info = self.action_list[lender_obj].lender_instance_info[self.action_list[lender_obj].share_count-1]
+                # #加容器状态检查
+                # self.action_list[lender_obj].lender_instance_info[self.action_list[lender_obj].share_count-1] = None
+                # self.action_list[lender_obj].share_count -= 1
+                # self.action_list[lender_obj].current_containers -= 1
+                # action_obj.current_containers += 1
+                lender_sign = True
+        else:lender_sign = False
+        return lender_sign
 
             
 test = node_controller(1)
