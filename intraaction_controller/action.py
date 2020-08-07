b3297caac76adff2f3cc17b5601c0906c05421b8
Jiagan Cheng <chnegjiagan@gmail.com>
2020-08-07 21:22:11 +0800

    finish intraaction controller

--- intraaction_controller/action.py
+++ intraaction_controller/action.py
@@ -1,12 +1,16 @@
 import docker
+import time
+import math
 from threading import Lock, Timer
 from container import Container
+from idle_container_algorithm import idle_status_check
 
 interval = 5 # repack and clean every 5 seconds
 timer = None
+update_rate = 0.85 # the update rate of lambda and mu
 
 class Action:
-    def __init__(self, client, action_name, pwd, port_manager, action_manager):
+    def __init__(self, client, action_name, pwd, port_manager, action_manager, database, qos_time, qos_requirement):
         self.client = client
         self.name = action_name
         self.port_manager = port_manager
@@ -14,6 +18,7 @@ class Action:
         self.pwd = pwd
         self.img_name = 'img_' + action_name
         self.pack_img_name = None
+        self.database = database
 
         # any modification of pool should first acquire pool's lock
         self.pool_lock = Lock()
@@ -25,6 +30,14 @@ class Action:
         global timer
         timer = Timer(interval, repack_and_clean, args=[self])
 
+        # statistical infomation for idle container identifying
+        self.lambd = -1
+        self.rec_mu = -1
+        self.qos_real = 0
+        self.qos_time = qos_time
+        self.qos_requirement = qos_requirement
+        self.request_log = []
+
     # receive a request from upper layer
     # the precedence of container source:
     #   1. action's executant pool
@@ -32,7 +45,9 @@ class Action:
     #   3. action's lender pool
     #   4. other actions' lender container
     #   5. create new container
-    def send_request(self, data):
+    def send_request(self, request_id, data):
+        start = time.time()
+
         # 1.1 try to get a workable container from pool
         container = self.self_container()
         # 1.2 try to get a renter container from interaction controller
@@ -48,7 +63,11 @@ class Action:
         self.put_container(container)
 
         # 4. write the result into database
-        # TODO: write the result into couchdb
+        self.database[request_id] = res
+
+        # update statistical infomation for idle
+        end = time.time()
+        self.request_log.append((start, end))
 
     # get a container from container pool
     # if there's no container in pool, return None
@@ -147,17 +166,77 @@ class Action:
     def init_container(self, container):
         container.init(self.name, self.pwd)
 
-def repack_and_clean(action):
-    # TODO: repack and clean
+    def update_statistics(self):
+        # do not update if no requests came
+        if len(self.request_log) == 0:
+            return
+
+        # sort all logs by start time
+        self.request_log.sort(key=lambda x: x[0])
+
+        intervals = [y[0] - x[0] for x, y in zip(self.request_log, self.request_log[1:])]
+        new_lambd = favg(intervals)
+        durations = [e - s for s, e in self.request_log]
+        new_rec_mu = favg(durations)
+
+        self.qos_real = sum([x < self.qos_time for x in durations]) / len(durations)
+        if self.lambd > 0:
+            self.lambd = update_rate * self.lambd + (1 - update_rate) * new_lambd
+        if self.rec_mu > 0:
+            self.rec_mu = update_rate * self.rec_mu + (1 - update_rate) * new_rec_mu
+
+        self.request_log = []
+
+def favg(a):
+    return math.fsum(a) / len(a)
+
+# life time of three different kinds of containers
+exec_lifetime = 60
+lender_lifetime = 120
+renter_lifetime = 40
+
+# do the repack and cleaning work regularly
+def repack_and_clean(action: Action):
+    # find the old containers
+    old_container = []
+    action.pool_lock.acquire()
+    action.exec_pool = clean_pool(action.exec_pool, exec_lifetime, old_container)
+    action.lender_pool = clean_pool(action.lender_pool, lender_lifetime, old_container)
+    action.renter_pool = clean_pool(action.renter_pool, renter_lifetime, old_container)
+    action.pool_lock.release()
+
+    # repack containers
+    action.update_statistics()
+    repack_container = None
+    action.pool_lock.acquire()
+    if len(action.exec_pool) > 0:
+        n = len(action.exec_pool) + len(action.lender_pool) + len(action.renter_pool)
+        idle_sign = idle_status_check(action.lambd, n, 1/action.rec_mu, action.qos_time, action.qos_real, action.qos_requirement)
+        if idle_sign:
+            repack_container = action.exec_pool.pop(0)
+    action.pool_lock.release()
+
+    # time consuming work is put here
+    for c in old_container:
+        c.destroy()
+    if repack_container is not None:
+        repack_container = action.repack_container(repack_container)
+        action.pool_lock.acquire()
+        action.lender_pool.append(repack_container)
+        action.pool_lock.release()
 
     global timer
     timer = Timer(interval, repack_and_clean, args=[action])
 
-
-# intra-action 接口
-# 1. 接受inter的repack消息, 需要销毁所有的lender容器
-# ps. container repack的时机由intra选择
-# 全局定时器定时检查可以repack的container和可以销毁的container
-# 2. 接受action run的请求
-# 3. 取走lender container
-# 4. 每种状态的容器的数量
+# the pool list is in order:
+# - at the tail is the hottest containers (most recently used)
+# - at the head is the coldest containers (least recently used)
+def clean_pool(pool, lifetime, old_container):
+    cur_time = time.time()
+    idx = 0
+    for i, c in enumerate(pool):
+        if cur_time - c.lasttime < lifetime:
+            idx = i
+            break
+    old_container.extend(pool[idx:])
+    return pool[:idx]
