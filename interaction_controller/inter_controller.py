12ef58e3ae57d59dd91745ad453aa146b895908a
Barrin <58480159+BarrinXu@users.noreply.github.com>
2021-12-24 13:29:49 +0800

    upd

--- interaction_controller/inter_controller.py
+++ interaction_controller/inter_controller.py
@@ -53,7 +53,7 @@ class inter_controller():
         virtualenv_path = './virtualenv/'
         init_file = open('init_venv.bash', 'w', encoding='utf-8')
         for action in self.all_packages:
-            init_file.write('virtualenv ' + virtualenv_path + action + '\n')
+            init_file.write('virtualenv --no-setuptools --no-pip --no-wheel ' + virtualenv_path + action + '\n')
             init_file.write('source ' + virtualenv_path + action + '/bin/activate\n')
             for package, version in self.all_packages[action].items():
                 init_file.write('pip3 install ' + package + '==' + version + '\n')
