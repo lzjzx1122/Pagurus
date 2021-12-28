bd29ef52fdb8768804f27ea47a4f068bee0e5018
Barrin <58480159+BarrinXu@users.noreply.github.com>
2021-12-28 19:59:31 +0800

    upd

--- aws/send_to_15000_docker.py
+++ aws/send_to_15000_docker.py
@@ -3,6 +3,6 @@ import requests
 
 base_url = 'http://127.0.0.1:{}/{}'
 
-data = {'action': sys.argv[1]}
-r = requests.post(base_url.format(15000, 'init'), json=data)
+data = {'action': sys.argv[2]}
+r = requests.post(base_url.format(sys.argv[1], 'init'), json=data)
 print(r)
