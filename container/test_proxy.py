bb216acce975c91b3adf80f4a5391e4682d7b417
Barrin <58480159+BarrinXu@users.noreply.github.com>
2021-12-28 21:08:02 +0800

    upd

--- container/test_proxy.py
+++ container/test_proxy.py
@@ -3,6 +3,7 @@ import sys
 import time
 
 st = time.time()
-os.system('"python3 sub_proxy.py" {}'.format(sys.argv[1]))
+# os.system('su -c "python3 sub_proxy.py" {}'.format(sys.argv[1]))
+os.system('python3 sub_proxy.py')
 ed = time.time()
 print(ed-st)
