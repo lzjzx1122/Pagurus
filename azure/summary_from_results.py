50923e2fcc228d92e990d9c396629289c3b119cd
LinsongGuo <gls1196@sjtu.edu.cn>
2022-06-05 11:13:26 +0800

    Update azure scripts

--- azure/summary_from_results.py
+++ azure/summary_from_results.py
@@ -5,22 +5,23 @@ import json
 import sys,os
 import pandas
 
-for root, dirs, files in os.walk('result/'):
+for root, dirs, files in os.walk('result-plot/'):
     array = dirs
     if array:
         D = array
         break
 D_len = len(D)
 
+
 # generate cold_start.csv
 def get_cold(kind):
     res = {}
     for dir in D:
-        filename = 'result/' + str(dir) + '/' + kind + '/result.csv'
+        filename = 'result-plot/' + str(dir) + '/' + kind + '/result.csv'
         print('now precessing number ', dir , 'for ', kind, 'of file cold_start.csv')
         df = pd.read_csv(filename)
         for idx, row in df.iterrows():
-            action = int(row['action'][7:]) * D_len - D_len + int(dir)
+            action = int(row['action'][7:]) * D_len + int(dir)
             if row['start'] == 'cold':
                 if action not in res:
                     res[action] = 0
@@ -41,14 +42,13 @@ rows = []
 #row = {'action': 'all', 'openwhisk': openwhisk_total, 'pagurus': pagurus_total}
 #rows.append(row)   
 
-for i in range(0,len(openwhisk)):
-    action = i
+for action in openwhisk:
     openwhisk_cold = openwhisk[action] if action in openwhisk else 0
     pagurus_cold = pagurus[action] if action in pagurus else 0
     row = {'action': action, 'openwhisk': openwhisk_cold, 'pagurus': pagurus_cold}
     rows.append(row)
-
-file_name = 'result/cold_start.csv'
+rows = sorted(rows,key=lambda x:x['action'])
+file_name = 'result-plot/cold_start.csv'
 with open(file_name, mode='w') as csv_file:
     fieldnames = ['action', 'openwhisk', 'pagurus']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
@@ -57,21 +57,19 @@ with open(file_name, mode='w') as csv_file:
         writer.writerow(row)
 
 
-
 # generate e2e_latency.csv
 def get_latency(kind):
     res = {}
     for dir in D:
-        filename = 'result/' + str(dir) + '/' + kind + '/result.csv'
+        filename = 'result-plot/' + str(dir) + '/' + kind + '/result.csv'
         print('now precessing number ', dir , 'for ', kind, 'of file e2e_latency.csv')
         df = pd.read_csv(filename)
         for idx, row in df.iterrows():
-            action = int(row['action'][7:]) * D_len - D_len + int(dir)
-            if row['start'] == 'cold':
-                if action not in res:
-                    res[action] = []
-                #res[action].append(float(row['end-to-end']))
-                res[action].append(float(row['end2end latency']))
+            action = int(row['action'][7:]) * D_len + int(dir)
+            #if row['container'] == 'rent' or row['container'] == 'create':
+            if action not in res:
+                res[action] = []
+            res[action].append(float(row['end2end latency']))
     return res
 
 def div(a, b):
@@ -84,16 +82,26 @@ openwhisk = get_latency('openwhisk')
 pagurus = get_latency('pagurus')
 
 rows = []
-for i in range(0, len(openwhisk)):
-    action = i
-    openwhisk_mean = openwhisk[action][max(0, int(0.95 * len(openwhisk[action])) - 1)] if action in openwhisk else None
-    pagurus_mean = pagurus[action][max(0, int(0.95 * len(pagurus[action])) - 1)]  if action in pagurus else None
-    row = {'action': action, 'openwhisk': openwhisk_mean, 'pagurus': pagurus_mean, 'pagurus/openwhisk': div(pagurus_mean, openwhisk_mean)}
+for action in openwhisk:
+    if action in openwhisk:
+        openwhisk[action] = sorted(openwhisk[action])
+        openwhisk_mean = openwhisk[action][max(0, int(0.95 * len(openwhisk[action])) - 1)] 
+    else:
+        openwhisk_mean = None
+    if action in pagurus:
+        pagurus[action] = sorted(pagurus[action])
+        pagurus_mean = pagurus[action][max(0, int(0.95 * len(pagurus[action])) - 1)] 
+    else:
+        pagurus_mean = None
+    
+    row = {'action': action, 'openwhisk': openwhisk_mean, 'pagurus': pagurus_mean, 'openwhisk/pagurus': div(openwhisk_mean, pagurus_mean)}
     rows.append(row)
 
-file_name = 'result/e2e_latency.csv'
+rows = sorted(rows,key=lambda x:x['action'])
+
+file_name = 'result-plot/e2e_latency.csv'
 with open(file_name, mode='w') as csv_file:
-    fieldnames = ['action', 'openwhisk', 'pagurus', 'pagurus/openwhisk']
+    fieldnames = ['action', 'openwhisk', 'pagurus', 'openwhisk/pagurus']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
     for row in rows:
