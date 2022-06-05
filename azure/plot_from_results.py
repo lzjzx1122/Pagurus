acb0b63c757be5d8a86990889dbcc974fb6a1195
LinsongGuo <gls1196@sjtu.edu.cn>
2022-06-05 11:13:54 +0800

    Update azure scripts

--- azure/plot_from_results.py
+++ azure/plot_from_results.py
@@ -11,7 +11,7 @@ fig, ax1 = plt.subplots()
 fig.set_size_inches(7,6)
 plt.rcParams.update({'font.size': 24})
 
-res = pd.read_csv('result/cold_start.csv')
+res = pd.read_csv('result-plot/cold_start.csv')
 #res = pd.read_csv('/content/cold_start.csv')
 y2 = res["openwhisk"].values
 for i in range(1, len(y2)):
@@ -38,7 +38,7 @@ plt.xlabel('Function ID', fontsize=28)
 plt.legend(fontsize=21, loc="upper left",ncol=2,handlelength=0.5)
 # fig.savefig("../figures/cold_new_1.pdf",bbox_inches='tight')
 # plt.show()
-fig.savefig("result/fig_14_a.pdf",bbox_inches='tight')
+fig.savefig("result-plot/fig_14_a.pdf",bbox_inches='tight')
 
 
 
@@ -49,7 +49,7 @@ ax1.set_xlim(0, 60)
 fig.set_size_inches(7,6)
 plt.rcParams.update({'font.size': 24})
 
-res = pd.read_csv('result/cold_start.csv')
+res = pd.read_csv('result-plot/cold_start.csv')
 #res = pd.read_csv('/content/alleviation.csv')
 total_function = len(res)
 # print(total_function)
@@ -95,7 +95,7 @@ ax1.set_ylim(0.2,1.1)
 plt.legend(fontsize=26, loc="lower right")
 # fig.savefig("../figures/cold_new_2.pdf",bbox_inches='tight')
 # plt.show()
-fig.savefig("result/fig_14_b.pdf",bbox_inches='tight')
+fig.savefig("result-plot/fig_14_b.pdf",bbox_inches='tight')
 
 
 
@@ -105,7 +105,7 @@ fig, ax1 = plt.subplots()
 fig.set_size_inches(16, 7)
 
 
-res = pd.read_csv('result/e2e_latency.csv')
+res = pd.read_csv('result-plot/e2e_latency.csv')
 
 y1 = res["pagurus"].values
 # print(len(y1))
@@ -116,7 +116,7 @@ ax1.tick_params(axis='y')
 ax1.scatter(x1,y1,label='Pagurus',color="#f5616f",s=16)
 # plt.yticks([0, 1, 2],[0, "10^0", "10^1"])
 
-y2 = res["pagurus/openwhisk"].values
+y2 = res["openwhisk/pagurus"].values
 # print(len(y2))
 for index in range(len(y2)):
     y2[index] = np.log10(y2[index])+1
@@ -134,4 +134,4 @@ ax1.legend(fontsize=30, loc="upper left", handlelength = 0.5,  markerscale = 3)
 ax2.legend(fontsize=30, loc="upper right", handlelength = 0.5,  markerscale = 3)
 # fig.savefig("../figures/overhead_1.pdf",bbox_inches='tight')
 # plt.show()
-fig.savefig("result/fig_15.pdf",bbox_inches='tight')
+fig.savefig("result-plot/fig_15.pdf",bbox_inches='tight')
